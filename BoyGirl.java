62a3c3e88c54eda9c1d0e48020b80cbed25a791b
Susan Driskill <56049351+susandriskill@users.noreply.github.com>
2020-02-16 00:33:29 -0800

    Adds BoyGirl.java
    Assignment 6, Page 439, Exercise 1

--- BoyGirl.java
+++ BoyGirl.java
@@ -24,57 +24,50 @@ public class BoyGirl {
 		Scanner input = new Scanner(new File("boysgirls.txt"));
 		//Test the boyGirl method with the file
 		boyGirl(input);
+		//The results should be: 
+		//4 boys, 3 girls
+		//Difference between boys' and girls' sums: 5
 	}
 
 	public static void boyGirl(Scanner input) {
-		//Declare and instantiate the variables
+		//Declare and initialize the variables
 		int numOfBoys = 0;
 		int sumOfBoys = 0;
 		int numOfGirls = 0;
 		int sumOfGirls = 0;
 
 		//The file always begins with a boy's name
-		
 		//Create a while loop to collect all of the information
-		
 		while (input.hasNext()) {
 			//Pass through the boy's name
 			input.next();
-			//Add 1 to the total number of boys for each boy 
+			//Add 1 to the total number of boys for each boy
 			//listed
-			numOfBoys = numOfBoys + 1;
-			//Add the integer after each boy's name to the 
-			//total sum
+			numOfBoys++;
+			//Add the integer after each boy's name to the total
+			//sum
 			sumOfBoys += input.nextInt();
-			
+
 			//In the list, there is a possibility of a boy not 
 			//followed by a girl, so create an if statement
-			
+
 			if (input.hasNext()) {
 				// Pass through the girl's name
 				input.next();
-				//Add 1 to the total number of girls for each 
+				//Add 1 to the total number of girls for each
 				//girl listed
-				numOfGirls = numOfGirls + 1;
+				numOfGirls++;
 				//Add the integer after each girl's name to the
 				//total sum
 				sumOfGirls += input.nextInt();
-			} else {
-				//Do nothing
 			}
 
 		}
 
 		//Declare the variable
-		int difference;
-		
-		//We do not know if the boys' or girls' sum will be 
-		//bigger, so we instantiate it depending on the results
-		if (sumOfBoys > sumOfGirls) {
-			difference = sumOfBoys - sumOfGirls;
-		} else {
-			difference = sumOfGirls - sumOfBoys;
-		}
+		//Use the absolute value of the difference of the sum of
+		//boys and the sum of the girls
+		int difference = Math.abs(sumOfBoys - sumOfGirls);
 		
 		System.out.println(numOfBoys + " boys, " + numOfGirls + " girls");
 		System.out.println("Difference between boys' and girls' sums: " + difference);
